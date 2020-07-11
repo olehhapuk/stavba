1f1f3d7096cf9dc8696317006b3f5f357573ec36
olehhapuk <oleggapuk20@gmail.com>
2020-07-11 11:55:06 +0300

    feat: users can now send emails

--- contacts/form.php
+++ contacts/form.php
@@ -1,30 +1,31 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../php-mailer/Exception.php';
-require '../php-mailer/PHPMailer.php';
-require '../php-mailer/SMTP.php';
+require '../vendor/autoload.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
+
+$email = 'user@mail.com';
 
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.mail.com';                        // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     // SMTP username
+    $mail->Username   = $email;                                 // SMTP username
     $mail->Password   = 'secret';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->setFrom($email, 'Mailer');
+    $mail->addAddress($email, 'Joe User');     // Add a recipient
+    $mail->addReplyTo($email, 'Information');
 
     if (!isset($_POST['user_name']) || !isset($_POST['user_number']) || !$_POST['user_message'])
     {
@@ -47,7 +48,6 @@ try {
     $mail->AltBody = $bodyAlt;
 
     $mail->send();
-    echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
